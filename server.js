811f7713e1a5d7ac9f808fd8db08a3dfd3673db2
NikitaRyabtsev2004 <117067066+NikitaRyabtsev2004@users.noreply.github.com>
2024-09-25 00:35:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 13000;
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
